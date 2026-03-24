2c1d5cf375e3a4f56daa87126bc724a1b74864c5
Aneeqah Talaaboedien <219099405@mycput.ac.za>
2026-03-24 23:54:06 +0200

    domain-user

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,66 @@
+package domain;
+
+public class User {
+    private String userId;
+    private String userName;
+    private String email;
+    private String password;
+
+
+    private User(){}
+
+    private User(Builder builder){
+        this.userId= builder.userId;
+        this.userName = builder.userName;
+        this.email = builder.email;
+        this.password = builder.password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getEmail() {return email;}
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String userId;
+        private String userName;
+        private String email;
+        private String password;
+
+        public Builder setUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+
+        }
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+        public User build() { return new User(this);}
+    }
+}
